0174e54346b07db051f4d7e433c7a2ee726580d1
Svetlana <svetlanalasiichyk@gmail.com>
2023-05-05 12:26:49 +0300

    ADD

---
+++ routes/userRouter.js
@@ -0,0 +1,24 @@
+const { Router } = require('express')
+const { userController } = require('../controller')
+
+const userRouter = Router()
+
+// TODO validation middleware
+
+userRouter
+  .route('/')
+  .get(userController.getUsers)
+  .post(userController.createUser)
+
+userRouter
+  .route('/:userId')
+  .get(userController.getUserById)
+  .patch(userController.updateUserById)
+  .delete(userController.deleteUserById)
+
+userRouter
+  .route('/:userId/posts')
+  .post(userController.createUserPost)
+  .get(userController.getUserPosts)
+
+module.exports = userRouter
